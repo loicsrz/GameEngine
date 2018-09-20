f000dbd8f0170569e56a1b10de9908abe61c08ed
Vincent <vincent.guillon@hotmail.fr>
2018-09-20 19:28:45 -0400

    Tuto2Wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,107 @@
 //
 // Created by Vince on 29/08/2018.
 //
-#include<iostream>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+
+#include <glm/glm.hpp>
+#include "shader.hpp"
+
+using namespace glm;
+
+int main( void ) {
+    // Initialise GLFW
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        getchar();
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Open a window and create its OpenGL context
+    window = glfwCreateWindow(1024, 768, "Playground", NULL, NULL);
+    if (window == NULL) {
+        fprintf(stderr,
+                "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+
+    // Assure que l'on peut capturer la touche d'échappement enfoncée ci-dessous
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    // Create and compile our GLSL program from the shaders
+    //GLuint programID = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader" );
+
+    // An array of 3 vectors which represents 3 vertices
+    static const GLfloat g_vertex_buffer_data[] = {
+            -1.0f, -1.0f, 0.0f,
+            1.0f, -1.0f, 0.0f,
+            0.0f,  1.0f, 0.0f,
+    };
+
+    // This will identify our vertex buffer
+    GLuint vertexbuffer;
+    // Generate 1 buffer, put the resulting identifier in vertexbuffer
+    glGenBuffers(1, &vertexbuffer);
+    // The following commands will talk about our 'vertexbuffer' buffer
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    // Give our vertices to OpenGL.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    do{
+        // 1rst attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+                0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+                3,                  // size
+                GL_FLOAT,           // type
+                GL_FALSE,           // normalized?
+                0,                  // stride
+                (void*)0            // array buffer offset
+        );
+        // Draw the triangle !
+        glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+
+        glDisableVertexAttribArray(0);
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    } // Vérifie si on a appuyé sur la touche échap (ESC) ou si la fenêtre a été fermée
+    while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+           glfwWindowShouldClose(window) == 0 );
+
+}
+/*#include<iostream>
 #include <vector>
 #include <GL/glut.h>
 #include "../include/2B3_Engine/Particle.h"
@@ -129,7 +229,8 @@ void vectorIntegrator(vector<Particle *> particles, float deltaTime) {
 
 //    int n = 1;
     for (auto &particle : particles) {
-        /*cout << "Particle " << n << " :" << endl;
+        */
+/*cout << "Particle " << n << " :" << endl;
         cout << "Avant Integration : " << endl;
         cout << "Position : " << endl;
         cout << '\t' << "x : " << (*it)->getPosition()->getX() << endl;
@@ -142,9 +243,11 @@ void vectorIntegrator(vector<Particle *> particles, float deltaTime) {
         cout << "Acceleration : " << endl;
         cout << '\t' << "x : " << (*it)->getAcceleration()->getX() << endl;
         cout << '\t' << "y : " << (*it)->getAcceleration()->getY() << endl;
-        cout << '\t' << "z : " << (*it)->getAcceleration()->getZ() << endl;*/
+        cout << '\t' << "z : " << (*it)->getAcceleration()->getZ() << endl;*//*
+
         particle->integrator(deltaTime);
-        /*cout << "Après Integration : " << endl;
+        */
+/*cout << "Après Integration : " << endl;
         cout << "Position : " << endl;
         cout << '\t' << "x : " << (*it)->getPosition()->getX() << endl;
         cout << '\t' << "y : " << (*it)->getPosition()->getY() << endl;
@@ -158,7 +261,8 @@ void vectorIntegrator(vector<Particle *> particles, float deltaTime) {
         cout << '\t' << "y : " << (*it)->getAcceleration()->getY() << endl;
         cout << '\t' << "z : " << (*it)->getAcceleration()->getZ() << endl;
         cout << endl;
-        n++;*/
+        n++;*//*
+
     }
 }
 
@@ -195,4 +299,4 @@ int main(int argc, char **argv) {
     glutDisplayInit(argc, argv);
 
     return 0;
-}
+}*/
