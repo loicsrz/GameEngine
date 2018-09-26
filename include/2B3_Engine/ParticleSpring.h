68ef15079a9b261d01e6c93806e8e34a8e50f3aa
Vincent <vincent.guillon@hotmail.fr>
2018-09-26 14:58:03 -0400

    ParticleSpring implementation

---
+++ include/2B3_Engine/ParticleSpring.h
@@ -0,0 +1,37 @@
+//
+// Created by Vince on 26/09/2018.
+//
+
+#ifndef GAMEENGINE_PARTICLESPRING_H
+#define GAMEENGINE_PARTICLESPRING_H
+
+
+#include "ParticleForceGenerator.h"
+
+class ParticleSpring : ParticleForceGenerator {
+
+protected:
+Particle secondParticle;
+float K;
+float l0;
+
+public:
+    ParticleSpring();
+    ~ParticleSpring();
+
+    void UpdateForce(Particle *particle, float frame_duration) override;
+
+    const Particle &getSecondParticle() const;
+
+    void setSecondParticle(const Particle &secondParticle);
+
+    float getK() const;
+
+    void setK(float K);
+
+    float getL0() const;
+
+    void setL0(float l0);
+};
+
+#endif //GAMEENGINE_PARTICLESPRING_H
---
+++ src/ParticleSpring.cpp
@@ -0,0 +1,40 @@
+//
+// Created by Vince on 26/09/2018.
+//
+
+#include "../include/2B3_Engine/ParticleSpring.h"
+
+ParticleSpring::ParticleSpring() {}
+
+ParticleSpring::~ParticleSpring() {}
+
+void ParticleSpring::UpdateForce(Particle *particle, float frame_duration) {
+    Vector3D *d = particle->getPosition()->substractVector(secondParticle.getPosition());
+    float coef = -K*(d->getNorm()-l0);
+    d = d->normalizeVector();
+    particle->addForce(d->scalarMultiplier(coef));
+}
+
+const Particle &ParticleSpring::getSecondParticle() const {
+    return secondParticle;
+}
+
+float ParticleSpring::getK() const {
+    return K;
+}
+
+float ParticleSpring::getL0() const {
+    return l0;
+}
+
+void ParticleSpring::setSecondParticle(const Particle &secondParticle) {
+    ParticleSpring::secondParticle = secondParticle;
+}
+
+void ParticleSpring::setK(float K) {
+    ParticleSpring::K = K;
+}
+
+void ParticleSpring::setL0(float l0) {
+    ParticleSpring::l0 = l0;
+}
