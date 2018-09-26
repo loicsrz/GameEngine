25407d9d5d8f2dcc2a781ab701ece563559fb9b3
Unknown <thomas.sassano@insa-lyon.fr>
2018-09-26 17:08:43 -0400

    Mise en place de ParticleFloating

---
+++ include/2B3_Engine/ParticleFloating.h
@@ -0,0 +1,43 @@
+//
+// Created by totoa on 26/09/2018.
+//
+
+#ifndef GAMEENGINE_PARTICLEFLOATING_H
+#define GAMEENGINE_PARTICLEFLOATING_H
+
+
+#include "ParticleForceGenerator.h"
+
+class ParticleFloating : ParticleForceGenerator {
+protected:
+    float depth;
+    float volume;
+    float liquidLevel;
+    float liquidDensity;
+
+public:
+    ParticleFloating();
+
+    virtual ~ParticleFloating();
+
+    void UpdateForce(Particle *particle, float frame_duration) override;
+
+    float getDepth() const;
+
+    void setDepth(float depth);
+
+    float getVolume() const;
+
+    void setVolume(float volume);
+
+    float getLiquidLevel() const;
+
+    void setLiquidLevel(float liquidLevel);
+
+    float getLiquidDensity() const;
+
+    void setLiquidDensity(float liquidDensity);
+};
+
+
+#endif //GAMEENGINE_PARTICLEFLOATING_H
