24178d2c4f339b84aa861ee18873d3c06af969ef
Loïc <ljedi007@gmail.com>
2018-11-07 15:36:49 -0500

    Add Quaternion.h and Quaternion.cpp

---
+++ include/2B3_Engine/Quaternion.h
@@ -0,0 +1,40 @@
+//
+// Created by loicsrz on 07/11/2018.
+//
+
+#ifndef GAMEENGINE_QUATERNION_H
+#define GAMEENGINE_QUATERNION_H
+
+
+#include "Vector3D.h"
+
+class Quaternion {
+
+protected:
+    float r;
+    float i;
+    float j;
+    float k;
+
+public:
+
+    Quaternion(float a, float b, float c, float d);
+
+    ~Quaternion();
+
+    void normalize();
+
+    void doRotation(Vector3D v);
+
+    void UpdateAngularVelocity(Vector3D v, float duration);
+
+    Quaternion operator*(Quaternion const &b);
+
+    Quaternion operator*(float const &number);
+
+    Quaternion operator+(Quaternion const &quaternion);
+
+};
+
+
+#endif //GAMEENGINE_QUATERNION_H
---
+++ src/Quaternion.cpp
@@ -0,0 +1,60 @@
+//
+// Created by loicsrz on 07/11/2018.
+//
+
+#include <cmath>
+#include "../include/2B3_Engine/Quaternion.h"
+
+void Quaternion::normalize() {
+
+    float d = r*r+i*i+j*j+k*k;
+    if(d == 0){
+        r = 1;
+        i=j=k=0;
+    } else{
+        d = 1/sqrt(d);
+        r = d*r;
+        i = d*i;
+        j = d*j;
+
+    }
+
+}
+
+void Quaternion::UpdateAngularVelocity(Vector3D v, float duration) {
+    (*this) = (*this) + (Quaternion(0,v.getX(),v.getY(), v.getZ())*(duration/2))*(*this);
+
+}
+
+Quaternion Quaternion::operator*(Quaternion const &b) {
+    float w = this->r*b.r - this->i*b.i - this->j*b.j - this->k*b.k;
+    float i = this->r*b.i + b.r*this->i + this->j*b.k - this->k*b.j;
+    float j = this->r*b.j + b.r*this->j + this->k*b.i - this->i*b.k;
+    float k = this->r*b.k + b.r*this->k + this->i*b.j - this->j*b.i;
+
+    Quaternion *quaternion = new Quaternion(w, i, j, k);
+    return *quaternion;
+}
+
+Quaternion::Quaternion(float a, float b, float c, float d):r(a),i(b),j(c),k(d) {
+
+}
+
+Quaternion::~Quaternion() {
+
+}
+
+void Quaternion::doRotation(Vector3D v) {
+
+    Quaternion *quaternion = new Quaternion(0,v.getX(),v.getY(),v.getZ());
+    (*this) = (*this)*(*quaternion);
+
+}
+
+Quaternion Quaternion::operator*(float const &number) {
+    return Quaternion(this->r*number, this->i*number, this->j*number, this->k*number);
+}
+
+Quaternion Quaternion::operator+(Quaternion const &quaternion) {
+    return Quaternion(this->r + quaternion.r, this->i + quaternion.i, this->j + quaternion.j, this->k + quaternion.k);
+}
