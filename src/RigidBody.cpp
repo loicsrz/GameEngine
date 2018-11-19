3c7f60f8b671a0040f2dc70617e127fcbe513f65
Loïc <ljedi007@gmail.com>
2018-11-19 15:11:34 -0500

    Add Constructor and destructor, comment lines and add TODO

--- src/RigidBody.cpp
+++ src/RigidBody.cpp
@@ -2,12 +2,36 @@
 // Created by loicsrz on 14/11/2018.
 //
 
+#include <cmath>
 #include "../include/2B3_Engine/RigidBody.h"
+
+/// Début Constructeurs/Destructeur ---------------------------------------------------------------------------------
+
+RigidBody::RigidBody(float invertedMass, float linearDamping, Vector3D *position, Vector3D *velocity, Quaternion *orientation,
+                     Vector3D *rotation, float angularDamping, Vector3D *forcesAccum, Vector3D *torqueAccum)
+                     : invertedMass(invertedMass), linearDamping(linearDamping), position(position), velocity(velocity),
+                     orientation(orientation), rotation(rotation), angularDamping(angularDamping), forcesAccum(forcesAccum)
+                     , torqueAccum(torqueAccum){
+
+}
+
+RigidBody::~RigidBody() {
+    delete this->position;
+    delete this->velocity;
+    delete this->orientation;
+    delete this->rotation;
+    this->forcesAccum = new Vector3D(0, 0, 0);
+    this->torqueAccum = new Vector3D(0, 0, 0);
+
+}
+
+/// Fin Constructeurs/Destructeur ---------------------------------------------------------------------------------
 
 void RigidBody::CalculDerivedData() {
 
 }
 
+/// Méthode visant à calculer les forces s'appliquant à un point (particule) du Corps Rigide
 void RigidBody::addForceAtPoint(Vector3D * Force, Vector3D * position) {
 
     // TODO : convertir la position en coordonnées relatives au centre de masse
@@ -25,25 +49,43 @@ void RigidBody::addForceAtBodyPoint(Vector3D * Force, Vector3D * position) {
 
 }
 
+/// Méthode d'ajout de force à la force accumulé par le Corps Rigide
 void RigidBody::addForceAccumulator(Vector3D *force) {
     forcesAccum = this->forcesAccum->addVector(force);
 }
 
+/// Méthode visant à calculer la position et la vélocité de la prochaine frame.
 void RigidBody::integrator(float time) {
 
+    Vector3D * acceleration = this->forcesAccum->scalarMultiplier(invertedMass);
+    // TODO : faire torqueAccum * moment d'inertie (qu'on trouve dans la fonction derivedData)
+    // TODO : update de la vélocité angulaire
+    UpdateSpeed(time, acceleration);
+    UpdatePosition(time);
+    // TODO : calculer derived data
+    clearAccumulator();
 }
 
+/// Methode de réinitialisation à 0 de la force accumulé et du torque accumulé par le Corps Rigide
 void RigidBody::clearAccumulator() {
     this->forcesAccum->setX(0.0f);
     this->forcesAccum->setY(0.0f);
     this->forcesAccum->setZ(0.0f);
 
+    // TODO : checker si torqueAccum doit être réinitialiser à chaque Frame
+    this->torqueAccum->setX(0.0f);
+    this->torqueAccum->setY(0.0f);
+    this->torqueAccum->setZ(0.0f);
+
 }
 
+/// Méthode d'ajout de torque au torque accumulé par le Corps Rigide
 void RigidBody::addTorqueAccumulator(Vector3D *torque) {
     torqueAccum = this->torqueAccum->addVector(torque);
 
 }
+
+/// Début de l'ensemble des getters et setters de la classe RigidBody ------------------------------------------------
 
 float RigidBody::getInvertedMass() const {
     return invertedMass;
@@ -108,3 +150,19 @@ void RigidBody::setOrientation(Quaternion *orientation) {
 void RigidBody::setRotation(Vector3D *rotation) {
     RigidBody::rotation = rotation;
 }
+
+/// Fin de l'ensemble des getters et setters de la classe RigidBody ------------------------------------------------
+
+
+/// Méthode de mise à jour de la vélocité du corps rigide.
+void RigidBody::UpdatePosition(float time) {
+    this->position = this->position->addVector(this->velocity->scalarMultiplier(time));
+}
+
+/// Méthode de mise à jour de la position du corps rigide.
+void RigidBody::UpdateSpeed(float time, Vector3D *acceleration) {
+    this->velocity = (this->velocity->scalarMultiplier(pow(this->linearDamping, time)))
+            ->addVector(acceleration->scalarMultiplier(time));
+}
+
+
