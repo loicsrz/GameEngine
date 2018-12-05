a7775feba99ca996afcf64a67ab972f84ba6c9c2
Vincent <vincent.guillon@hotmail.fr>
2018-12-05 14:54:07 -0500

    Plane updated

---
+++ include/2B3_Engine/Plane.h
@@ -0,0 +1,19 @@
+//
+// Created by vince on 05/12/2018.
+//
+
+#ifndef GAMEENGINE_PLANE_H
+#define GAMEENGINE_PLANE_H
+
+
+#include "Primitive.h"
+
+class Plane : public Primitive{
+
+    Vector3D * perpendicularAngle;
+    float offset;
+
+};
+
+
+#endif //GAMEENGINE_PLANE_H
---
+++ src/Plane.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vince on 05/12/2018.
+//
+
+#include "../include/2B3_Engine/Plane.h"
